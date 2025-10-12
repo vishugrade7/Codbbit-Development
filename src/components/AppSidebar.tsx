03aa11b9eb19c84e3f27982924d6f12d55dcf649
Vishu Grade <gradevishu@gmail.com>
2025-10-12 13:40:00 +0000

    remove (_for element div_)

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -21,7 +21,6 @@ import Image from 'next/image';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { usePathname, useRouter } from 'next/navigation';
 import { Skeleton } from './ui/skeleton';
-import { MusicToggleButton } from './MusicToggleButton';
 
 
 export function AppSidebar() {
@@ -121,7 +120,6 @@ export function AppSidebar() {
       </SidebarContent>
 
       <SidebarFooter className="mt-auto flex flex-col items-center gap-2 p-2">
-        <MusicToggleButton />
         <ThemeToggle />
          <TooltipProvider delayDuration={0}>
             <Tooltip>
