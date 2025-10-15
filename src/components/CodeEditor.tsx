f5dd14968c42d6086fa663e10c3b187b1ed0d5b7
Vishu Grade <gradevishu@gmail.com>
2025-10-15 00:00:40 +0000

    An error occurred in your build error in createBuildConfig: error in val

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -5,13 +5,6 @@ import Editor, { type Monaco, type OnChange, loader } from '@monaco-editor/react
 import { editor } from 'monaco-editor';
 import { useEffect, useState } from 'react';
 
-loader.config({
-  paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.49.0/min/vs'
-  }
-});
-
-
 interface CodeEditorProps {
   value: string;
   onChange: OnChange;
