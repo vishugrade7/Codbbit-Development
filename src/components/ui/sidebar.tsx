e279eed444b8e47bde9b73982e4f871498229989
Vishu Grade <gradevishu@gmail.com>
2025-10-19 05:29:22 +0000

    fix sidebar UI align items in a line (_for element <SidebarMenu>_)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -417,7 +417,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 items-center",
+        "flex min-h-0 flex-1 flex-col items-center gap-2",
         className
       )}
       {...props}
