bc3cb55038024c0d8094572446c8550b9c7a944e
Vishu Grade <gradevishu@gmail.com>
2025-10-19 07:41:10 +0000

    change to settings icon (_for element <Button>_)

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -125,7 +125,7 @@ export function AppSidebar() {
                   <TooltipTrigger asChild>
                     <DialogTrigger asChild>
                       <Button variant="ghost" size="icon" className="h-10 w-10">
-                            <MessageSquare size={20}/>
+                            <Settings size={20}/>
                       </Button>
                     </DialogTrigger>
                   </TooltipTrigger>
