eae22ea90e16020a58b5aa9af357dcd46bcd4cbf
Vishu Grade <gradevishu@gmail.com>
2025-10-19 04:44:30 +0000

    move this card upward in mobile screen (_for element <CardHeader>_)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from '@/components/AuthForm';
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start md:justify-center bg-background p-4 pt-20">
+    <div className="flex min-h-screen flex-col items-center justify-start md:justify-center bg-background p-4 pt-16">
       <AuthForm type="signup" />
     </div>
   );
