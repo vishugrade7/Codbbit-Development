9635db710ce44b0e4aa88d8398402f3757e8b3e5
Vishu Grade <gradevishu@gmail.com>
2025-10-19 05:05:44 +0000

    only take 3px margings around (_for element <CardHeader>_)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from '@/components/AuthForm';
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start md:justify-center bg-background p-4 pt-8 md:pt-4">
+    <div className="flex min-h-screen flex-col items-center justify-start md:justify-center bg-background p-1">
       <AuthForm type="signup" />
     </div>
   );
