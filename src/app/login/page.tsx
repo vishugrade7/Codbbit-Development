166e1bd7bb925e9beafdc57624d7187a5693b53e
Vishu Grade <gradevishu@gmail.com>
2025-10-19 04:43:17 +0000

    move this card upward in mobile screen (_for element <Card>_)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from '@/components/AuthForm';
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background pt-16">
+    <div className="flex min-h-screen flex-col items-center justify-start md:justify-center bg-background pt-16">
       <AuthForm type="login" />
     </div>
   );
